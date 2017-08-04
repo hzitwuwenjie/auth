82b5c2426f518d3d30244681c70e7427a131492a
峰 <1264291059@qq.com>
2017-08-04 11:22:37 +0800

    修改web目录

--- auth-web/src/main/java/com/hzit/servlet/ToLoginServlet.java
+++ auth-web/src/main/java/com/hzit/servlet/ToLoginServlet.java
@@ -1,6 +1,9 @@
 package com.hzit.servlet;
 
 import com.hzit.dao.UserInfoDao;
+import com.hzit.entity.UserInfo;
+import com.hzit.util.SqlSessionHelper;
+import org.apache.ibatis.session.SqlSession;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,7 +17,22 @@ import java.io.IOException;
 @WebServlet(name = "LoginServlet",urlPatterns = "/login")
 public class ToLoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        SqlSession sqlSession= SqlSessionHelper.getSqlSession();
+        UserInfoDao userInfoDao=sqlSession.getMapper(UserInfoDao.class);
+        UserInfo user=new UserInfo();
+        String userName =request.getParameter("username");
+        String userPwd=request.getParameter("password");
+        user.setuName(userName);
+        user.setuPass(userPwd);
+        UserInfo userInfo=userInfoDao.findUserById(user);
+        if(userInfo !=null){
+            request.getSession().setAttribute("user",userInfo);
+            response.sendRedirect("index.html");
+        }else{
+            response.sendRedirect("login.html");
+        }
     }
 
     protected void doGet(HttpServletRequest request,HttpServletResponse response) throws javax.servlet.ServletException, IOException {
